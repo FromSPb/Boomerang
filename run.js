31d268d3a148e9416a7d3424b04d13bca0557a64
snow482 <a.dolin125@gmail.com>
2024-11-01 13:54:50 +0300

    добавлена обработка запуска, обработка клавиатуры и часть логики игры

--- run.js
+++ run.js
@@ -1,12 +1,25 @@
 // Основной файл.
 // Запускает игру.
 const Game = require('./src/Game');
+const View = require('./src/View');
+
+// Создание экземпляра класса View
+const view = new View();
+// Вызов метода rega() из класса View и запуск игры после завершения
+view.rega()
+  .then((answers) => {
+    console.log('Выбор игрока:', answers);
 
 // Инициализация игры с настройками.
 const game = new Game({
   trackLength: 30,
+  heroSkin: answers.scins,
+  heroWeapon: answers.weapon,
 });
 
-
-// Запуск игры.
-game.play();
+  // Запуск игры.
+  game.play();
+})
+  .catch((error) => {
+    console.error('Ошибка при вызове rega():', error);
+  });
